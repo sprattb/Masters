64102af46af3c79ae9d156a26f1187fe0b2053ab
Jamie <n9360204@qut.edu.au>
2020-03-13 15:09:34 +1000

    first progress

--- MIP.jl
+++ MIP.jl
@@ -1,9 +1,19 @@
-using JuMP, GLPK
-
+using JuMP, GLPK, CSV, DataFrames
+#JuMP to construct problem
+#GLPK as solver
+#CSV for data imput and output
+#DataFrames for type
 """ first pass at implimenting MIP """
+NumPatients = 20
+numSpecialists = 6
+appointmentLengths = [1,2,3]
+numAppoints = 80
+lengthOfDay =50;
+clinicMIP = Model(GlPK.Optimizer)
 
+@variable(clinicMIP, strictlyLater[1:numAppoints,1:numAppoints], Bin)
+@variable(clinicMIP, 0<=startingTime[1:numAppoints]<=lengthOfDay,Int)
 
-function Clinic(numSpecialists, numPatients)
-    clinicMIP = Model(GLPK.Optimizer)
-    @variable(clinicMIP
-end
+@constraints(clinicMIP, begin
+    #con 1 -
+end)
--- data_Generation.jl
+++ data_Generation.jl
@@ -1,6 +1,41 @@
-""" Generates the data """
+using CSV, DataFrames
+#CSV for file writing
+#DataFrames for DataFrame type
 
-NumPatients = 20;
-numDoctors = 6;
+
+"""generates test data"""
+#default variables
+NumPatients = 20
+numSpecialists = 6
 appointmentLengths = [1,2,3]
+numAppoints = 80
+function junk()
+
+    files = ["a.txt", "b.txt", "c.txt"]
+    fvars = Dict()
+    for (n, f) in enumerate(files)
+        fvars["x_$(n)"] = f
+    end
+
+
+    #generate TestInfo file
+    PatientInfo = [0; NumPatients]
+    SpecialistInfo = [0; numSpecialists]
+    AppointmentInfo = [1; appointmentLengths]
+    TestCasesInfo = [0; numAppoints]
+
+    TestInfo = DataFrame(PatientInfo = PatientInfo, SpecialistInfo = SpecialistInfo, AppointmentInfo = AppointmentInfo, TestCasesInfo = TestCasesInfo)
+    CSV.write("testInfo.csv", TestInfo)
+end
+
+
+#generating test case
+SpecialistID= rand(1:1:numSpecialists,numAppoints)
+AppointmentLength = rand(appointmentLengths,numAppoints)
 #only allowing patients to have one appointment
+#PatientID=rand(1:1:numPatients,numAppoints)
+PatientID= 1:1:numAppoints
+
+#writing test case to file
+Appointment = DataFrame(SpecialistID = SpecialistID,AppointmentLength = AppointmentLength, PatientID = PatientID)
+CSV.write("testData.csv",Appointment)
